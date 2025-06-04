8bd833da2e29fbe8428599d550628acaa3112649
Danilo Silva <danilo.silva@msn.com>
2025-06-04 14:16:07 -0300

    feat: Adicionar comentário no componente App.jsx
    - Incluído um comentário explicativo sobre a sintaxe de comentários em JSX no componente App.jsx.

--- Matheus Battisti/React do Zero a Maestria/02_FUNDAMENTOS/fundamentos/src/App.jsx
+++ Matheus Battisti/React do Zero a Maestria/02_FUNDAMENTOS/fundamentos/src/App.jsx
@@ -7,6 +7,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      {/* Esta é a forma de comentar no JSX */}
       <h1>Fundamentos React</h1>
       <FirstComponent />
     </div>
