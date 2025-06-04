4064fee66438e53017247876d176063daa3861ba
Danilo Silva <danilo.silva@msn.com>
2025-06-04 11:59:25 -0300

    feat: Atualizar estrutura do projeto "desafio-1" com melhorias na configuração e novos componentes
    - Alterado o idioma do HTML para português (pt-BR) e atualizado o título da aplicação.
    - Removido o arquivo App.js e criado App.jsx com uma nova estrutura de componentes.
    - Adicionada uma nova folha de estilo para h3 com cor chocolate.
    - Criado arquivo de teste para o componente App.
    - Ajustadas as fontes no CSS para consistência e clareza.

---
+++ Matheus Battisti/React do Zero a Maestria/desafio-1/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello React</h1>
+        <h3>Vamos ver do que essa belezinha é capaz</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
