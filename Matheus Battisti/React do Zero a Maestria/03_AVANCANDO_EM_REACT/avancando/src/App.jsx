9b95ef7ccc4a2019536e630d04da217db3158771
Danilo Silva <danilo.silva@msn.com>
2025-06-05 08:21:25 -0300

    feat: Melhorar a lógica de manipulação de eventos no componente Events
    - Refinada a lógica de estado no componente Events para otimizar a interação do usuário.
    - Adicionadas novas funções de manipulação de eventos para aprimorar a experiência do usuário ao interagir com os eventos.

---
+++ Matheus Battisti/React do Zero a Maestria/03_AVANCANDO_EM_REACT/avancando/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Avançando em React</h1>
+      <h2>Imagens Públicas</h2>
+      <img src="/img1.jpg" alt="Paisagem" />
+    </div>
+  );
+}
+
+export default App;
