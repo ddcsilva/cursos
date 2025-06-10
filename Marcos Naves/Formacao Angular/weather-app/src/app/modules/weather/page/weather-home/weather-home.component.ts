3d5c2352b5d3fe76a63fd0d9659a207ca08af919
Danilo Silva <danilo.silva@msn.com>
2025-06-09 23:30:42 -0300

    feat: Implementar busca de localização no componente WeatherHome
    - Adicionada funcionalidade de busca de localização através de um formulário no template do WeatherHome.
    - Implementada a lógica para buscar dados meteorológicos com base na cidade informada pelo usuário.
    - Integrado o ícone de busca do FontAwesome ao componente para melhorar a interface do usuário.
    - Adicionada limpeza do campo de entrada após a submissão do formulário.

--- Marcos Naves/Formacao Angular/weather-app/src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ Marcos Naves/Formacao Angular/weather-app/src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
 import { WeatherData } from '../../models/interfaces/weather.interface';
+import { Subject, takeUntil } from 'rxjs';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
 })
-export class WeatherHomeComponent implements OnInit {
+export class WeatherHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
   initialCity = 'Jacareí';
   weatherData!: WeatherData;
+  searchIcon = faMagnifyingGlass;
 
   constructor(private weatherService: WeatherService) {}
 
@@ -17,14 +21,27 @@ export class WeatherHomeComponent implements OnInit {
   }
 
   getWeatherData(city: string): void {
-    this.weatherService.getWeatherData(city).subscribe({
-      next: (response) => {
-        response && (this.weatherData = response);
-        console.log(this.weatherData);
-      },
-      error: (error) => {
-        console.error('Error fetching weather data:', error);
-      },
-    });
+    this.weatherService
+      .getWeatherData(city)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          response && (this.weatherData = response);
+          console.log(this.weatherData);
+        },
+        error: (error) => {
+          console.error('Error fetching weather data:', error);
+        },
+      });
+  }
+
+  onSubmit(): void {
+    this.getWeatherData(this.initialCity);
+    this.initialCity = '';
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
