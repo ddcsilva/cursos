6bd39e2e49ad9d23211a6ce173574dd1199cd7d5
Danilo Silva <danilo.silva@msn.com>
2025-06-10 13:04:06 -0300

    feat: Integrar PrimeNG e aprimorar configuração do projeto Angular
    - Adicionadas dependências do PrimeNG, PrimeIcons e PrimeFlex ao projeto para melhorar a interface do usuário.
    - Atualizado o angular.json para incluir novos estilos do PrimeNG.
    - Modificado o app.component.ts para habilitar o efeito de ripple do PrimeNG.
    - Atualizado o app.module.ts para incluir módulos necessários como BrowserAnimationsModule e ReactiveFormsModule.

--- Marcos Naves/Formacao Angular/stock-control/src/app/app.module.ts
+++ Marcos Naves/Formacao Angular/stock-control/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
