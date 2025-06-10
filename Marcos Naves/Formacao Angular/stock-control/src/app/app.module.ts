51fd803012f14f00a197670cbab00b7def9a23e7
Danilo Silva <danilo.silva@msn.com>
2025-06-10 19:12:37 -0300

    feat: Implementar componente Home com funcionalidades de login e cadastro
    - Criado o componente Home com formulários para login e criação de conta, utilizando PrimeNG para estilização.
    - Adicionadas dependências do PrimeNG no app.module.ts, incluindo Card, InputText, Button e Toast.
    - Implementado o layout responsivo e estilização no home.component.scss, com uma imagem de fundo.
    - Adicionado arquivo de teste para o componente Home, garantindo a criação correta do componente.

--- Marcos Naves/Formacao Angular/stock-control/src/app/app.module.ts
+++ Marcos Naves/Formacao Angular/stock-control/src/app/app.module.ts
@@ -6,15 +6,24 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/home.component';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
