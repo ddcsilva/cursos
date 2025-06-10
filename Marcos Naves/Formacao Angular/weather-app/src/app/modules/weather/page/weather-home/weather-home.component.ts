422cfaf0a1d6560781439127e0b6c741142d6352
Danilo Silva <danilo.silva@msn.com>
2025-06-09 23:17:33 -0300

    feat: Adicionar interface WeatherData e implementar lógica no componente WeatherHome
    - Criada a interface WeatherData para estruturar os dados meteorológicos.
    - Implementada a lógica no componente WeatherHome para buscar e exibir dados do clima utilizando o WeatherService.
    - Adicionados novos arquivos de imagem para representar diferentes condições climáticas.

--- Marcos Naves/Formacao Angular/weather-app/src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ Marcos Naves/Formacao Angular/weather-app/src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,7 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { WeatherData } from '../../models/interfaces/weather.interface';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
 })
-export class WeatherHomeComponent {}
+export class WeatherHomeComponent implements OnInit {
+  initialCity = 'Jacareí';
+  weatherData!: WeatherData;
+
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.getWeatherData(this.initialCity);
+  }
+
+  getWeatherData(city: string): void {
+    this.weatherService.getWeatherData(city).subscribe({
+      next: (response) => {
+        response && (this.weatherData = response);
+        console.log(this.weatherData);
+      },
+      error: (error) => {
+        console.error('Error fetching weather data:', error);
+      },
+    });
+  }
+}
