7ab3e7e905d6ea8a2e486f4d63995e0bad3c5729
Danilo Silva <danilo.silva@msn.com>
2025-06-11 09:14:17 -0300

    feat: Adicionar componente Dashboard e atualizar rotas de navegação
    - Criado o componente Dashboard com seu respectivo módulo e rotas, permitindo a navegação para a página do dashboard.
    - Atualizadas as rotas no app-routing.module.ts para redirecionar para o dashboard ao acessar a raiz do aplicativo.
    - Estilizado o layout do componente de login e do dashboard para garantir uma apresentação responsiva e consistente.
    - Implementados testes básicos para o DashboardComponent, assegurando sua funcionalidade correta.

--- Marcos Naves/Formacao Angular/stock-control/src/app/app-routing.module.ts
+++ Marcos Naves/Formacao Angular/stock-control/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/components/login.component';
+import { DashboardComponent } from './features/dashboard/pages/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
   },
 ];
 
---
+++ Marcos Naves/Formacao Angular/stock-control/src/app/features/dashboard/dashboard.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    DashboardComponent
+  ],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+})
+export class DashboardModule {}
