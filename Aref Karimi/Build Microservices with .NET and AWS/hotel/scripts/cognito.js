31dbfe9582b0b52de56df64e0870d7e7e888e580
Danilo Silva <danilo.silva@msn.com>
2025-06-15 00:20:07 -0300

    feat: Adicionar funcionalidade de adição de hotéis e atualizar textos em português
    - Criada a página adicionarHotel.html para permitir a adição de novos hotéis ao sistema de gestão hoteleira.
    - Atualizados os textos e títulos nas páginas admin.html e index.html para o português, melhorando a acessibilidade.
    - Refatorada a lógica de autenticação nos scripts cognito.js e main.js para utilizar um objeto de dados do usuário autenticado.
    - Implementadas melhorias na estrutura HTML e na lógica de manipulação de formulários para uma melhor experiência do usuário.

--- Aref Karimi/Build Microservices with .NET and AWS/hotel/scripts/cognito.js
+++ Aref Karimi/Build Microservices with .NET and AWS/hotel/scripts/cognito.js
@@ -1,3 +1,7 @@
+/*
+  Propósito: Arquivo de configuração e inicialização do Amazon Cognito Auth SDK.
+*/
+
 const config = {
   cognito: {
     userPoolId: "us-east-2_EkGyR36BO",
@@ -10,19 +14,22 @@ var cognitoApp = {
   auth: {},
   Init: function () {
     var authData = {
-      ClientId: config.cognito.appId,
-      AppWebDomain: config.cognito.cognitoDomain,
-      TokenScopesArray: ["email", "openid", "profile"],
-      RedirectUriSignIn: "http://localhost/hotel/",
-      RedirectUriSignOut: "http://localhost/hotel/",
-      UserPoolId: config.cognito.identityPoolId,
-      AdvancedSecurityDataCollectionFlag: false,
-      Storage: null,
+      ClientId: config.cognito.appId, // ID do aplicativo do Cognito
+      AppWebDomain: config.cognito.cognitoDomain, // Domínio do Cognito
+      TokenScopesArray: ["email", "openid", "profile"], // Escopos do token
+      RedirectUriSignIn: "http://localhost/hotel/", // URL de redirecionamento após login
+      RedirectUriSignOut: "http://localhost/hotel/", // URL de redirecionamento após logout
+      UserPoolId: config.cognito.identityPoolId, // ID do pool de usuários do Cognito
+      AdvancedSecurityDataCollectionFlag: false, // Flag de coleta de dados avançada
+      Storage: null, // Armazenamento de dados (Usa localStorage padrão)
     };
 
+    // Inicializa o Cognito Auth
     cognitoApp.auth = new AmazonCognitoIdentity.CognitoAuth(authData);
+
+    // Define o manipulador de eventos do usuário
     cognitoApp.auth.userhandler = {
-      onSuccess: function (result) {},
+      onSuccess: function (result) {}, // Função chamada quando o login é bem-sucedido
       onFailure: function (err) {},
     };
   },
--- Aref Karimi/Build Microservices with .NET and AWS/hotel/scripts/main.js
+++ Aref Karimi/Build Microservices with .NET and AWS/hotel/scripts/main.js
@@ -1,50 +1,73 @@
-const currentUserToken = {
+/*
+  Arquivo principal que contém toda a lógica de autenticação, manipulação de tokens e interação com a UI.
+*/
+
+const dadosUsuarioAutenticado = {
   currentUserId: "",
   idToken: "",
   role: "",
 };
-function pageLoad() {
+
+/**
+ * Função para carregar os dados do usuário autenticado
+ */
+function inicializarAutenticacao() {
+  // 1. Processa resposta do Cognito na URL
   cognitoApp.auth.parseCognitoWebResponse(window.location.href);
+
+  // 2. Obtém usuário atual do localStorage
   var currentUser = cognitoApp.auth.getCurrentUser();
 
+  // 3. Se usuário existir, obtém sessão e tokens
   if (currentUser) {
     cognitoApp.auth.getSession();
-
     currentSession = cognitoApp.auth.signInUserSession;
 
-    currentUserToken.currentUserId = currentUser;
-    currentUserToken.idToken = currentSession.idToken.jwtToken;
-    console.info(currentUserToken);
+    // 4. Extrai informações do token
+    dadosUsuarioAutenticado.currentUserId = currentUser;
+    dadosUsuarioAutenticado.idToken = currentSession.idToken.jwtToken;
 
-    var tokenDetails = parseJwt(currentSession.idToken.jwtToken);
+    console.info(dadosUsuarioAutenticado);
+
+    // 5. Decodifica token para obter roles
+    var tokenDetails = decodificarTokenJwt(currentSession.idToken.jwtToken);
     if (tokenDetails["cognito:groups"]) {
       var groups = tokenDetails["cognito:groups"][0];
-      currentUserToken.role = groups;
+      dadosUsuarioAutenticado.role = groups;
     }
   }
 
+  // 6. Configura evento de clique para o botão de login
   $("#btnSignIn").on("click", function (btn) {
     cognitoApp.auth.getSession();
   });
 
+  // 7. Configura evento de clique para o botão de logout
   $("#btnSignOut").on("click", function (btn) {
-    currentUserToken.role = "";
-    currentUserToken.idToken = "";
-    currentUserToken.currentUserId = "";
+    dadosUsuarioAutenticado.role = "";
+    dadosUsuarioAutenticado.idToken = "";
+    dadosUsuarioAutenticado.currentUserId = "";
     cognitoApp.auth.signOut();
   });
 
+  // 8. Oculta os botões de login e logout
   $("#btnSignOut").hide();
   $("#btnSignIn").hide();
 
-  if (currentUserToken.currentUserId === "") {
+  // 9. Mostra os botões de login e logout
+  if (dadosUsuarioAutenticado.currentUserId === "") {
     $("#btnSignIn").show();
   } else {
     $("#btnSignOut").show();
   }
 }
 
-function parseJwt(token) {
+/**
+ * Função para decodificar o token JWT
+ * @param {string} token - Token JWT a ser decodificado
+ * @returns {Object} - Objeto JSON contendo os dados do token
+ */
+function decodificarTokenJwt(token) {
   var base64Url = token.split(".")[1];
   var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
   var jsonPayload = decodeURIComponent(
@@ -60,27 +83,37 @@ function parseJwt(token) {
   return JSON.parse(jsonPayload);
 }
 
-function setAuthHeader() {
+/**
+ * Função para configurar o cabeçalho de autenticação
+ */
+function configurarCabecalhoAutenticacao() {
   var forms = $("form");
   forms.submit(function (event) {
-    // Stop the form from submitting normally
+    // 1. Impede o envio normal do formulário
     event.preventDefault();
 
-    // Get form data
+    // 2. Obtém os dados do formulário
     var formData = new FormData($(this)[0]);
     console.log(formData);
-    // Set up the AJAX request
+
+    // 3. Configura a requisição AJAX
     $.ajax({
       url: $(this).attr("action"),
       type: "POST",
+      // 4. Configura o cabeçalho de autenticação
       beforeSend: function (request) {
-        request.setRequestHeader("Authorization", "Bearer " + currentUserToken.idToken);
+        request.setRequestHeader("Authorization", "Bearer " + dadosUsuarioAutenticado.idToken);
         request.setRequestHeader("Content-type", "multipart/form-data");
       },
+      // 5. Configura os dados do formulário
       data: formData,
+      // 6. Configura o tipo de conteúdo
       contentType: true,
+      // 7. Configura o processamento de dados
       processData: false,
+      // 8. Configura a função de sucesso
       success: function (response) {
+        // 9. Redireciona para a página de administração
         window.location.href = "/hotel/admin.html";
       },
     });
