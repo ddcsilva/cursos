1c7b176ec84bb440732b3fba99e2b754893fa432
Danilo Silva <danilo.silva@msn.com>
2025-06-20 19:05:02 -0300

    feat: Adicionar configuração inicial do projeto Express com dependências e rotas básicas
    - Criado arquivo .gitignore para ignorar a pasta node_modules.
    - Adicionado package-lock.json para gerenciar as dependências do projeto.
    - Atualizado package.json para incluir o script de desenvolvimento com nodemon e a dependência nodemon.
    - Implementadas rotas básicas no servidor para responder a requisições em diferentes endpoints.

--- Alura/node-express-mongo/server.js
+++ Alura/node-express-mongo/server.js
@@ -4,7 +4,14 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "content-type": "text/plain" });
+  res.end(rotas[req.url]);
 });
+
+const rotas = {
+  "/": "Curso de Express API",
+  "/livros": "Entrei na rota livros",
+  "/autores": "Entrei na rota autores",
+};
 
 server.listen(PORT, () => {
   console.log("Escutando");
