81239fa9a7e36069a3bb39b38b8968c2fbe4c198
Danilo Silva <danilo.silva@msn.com>
2025-06-21 12:12:38 -0300

    feat: Integrar Express ao projeto e refatorar servidor
    - Adicionada a dependência do Express no package.json e package-lock.json.
    - Criado o arquivo src/app.js para definir rotas e lógica do servidor.
    - Refatorado o server.js para utilizar o Express, simplificando a estrutura do servidor e melhorando a legibilidade do código.

--- Alura/node-express-mongo/server.js
+++ Alura/node-express-mongo/server.js
@@ -1,18 +1,7 @@
-import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/plain" });
-  res.end(rotas[req.url]);
-});
-
-const rotas = {
-  "/": "Curso de Express API",
-  "/livros": "Entrei na rota livros",
-  "/autores": "Entrei na rota autores",
-};
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("Escutando");
 });
