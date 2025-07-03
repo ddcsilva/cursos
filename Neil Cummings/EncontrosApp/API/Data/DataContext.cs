3d36cb96a1aa0d08475b4c37d8392caf406a3dd1
Danilo Silva <danilo.silva@msn.com>
2025-07-02 21:17:15 -0300

    feat: Remover arquivos e estrutura do projeto EncontrosApp
    - Excluídos arquivos da solução EncontrosApp.sln e do projeto API, incluindo configurações, controladores, entidades e arquivos de banco de dados.
    - Removidos arquivos de configuração do cliente Angular, incluindo .gitignore, package.json e arquivos de estilo.
    - Limpeza completa da estrutura do projeto, preparando para uma nova abordagem ou reestruturação.

--- Neil Cummings/EncontrosApp/API/Data/DataContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using API.Entities;
-
-namespace API.Data;
-
-public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
-{
-    public DbSet<Usuario> Usuarios { get; set; }
-}
--- Neil Cummings/EncontrosApp/API/Entities/Usuario.cs
+++
@@ -1,7 +0,0 @@
-namespace API.Entities;
-
-public class Usuario
-{
-    public int Id { get; set; }
-    public required string Nome { get; set; }
-}
