fed29e81a117e86929cc51e32484337951b4f646
Danilo Silva <danilo.silva@msn.com>
2025-07-03 23:12:12 -0300

    feat: Adicionar suporte a requisições HTTP no cliente Angular
    - Incluída a configuração do HttpClient no app.config.ts para permitir comunicação com APIs.
    - Implementada a lógica de requisição no componente App, que busca usuários da API e exibe os resultados no console.
    - Alterado o título da aplicação para 'Dating App'.

--- Neil Cummings/EncontrosApp/client/src/app/app.ts
+++ Neil Cummings/EncontrosApp/client/src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected title = 'client';
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  protected title = 'Dating App';
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/usuarios').subscribe({
+      next: (response) => console.log(response),
+      error: (error) => console.error(error),
+      complete: () => console.log('Requisição realizada com sucesso'),
+    });
+  }
 }
