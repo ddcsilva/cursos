2a94679108777ce8833d5e581d5c3633a98e0692
Danilo Silva <danilo.silva@msn.com>
2025-07-03 23:20:42 -0300

    feat: Atualizar configuração do .gitignore e adicionar suporte a CORS
    - Reduzido o conteúdo do arquivo .gitignore para incluir apenas entradas relevantes para o projeto Angular e arquivos de log.
    - Adicionado suporte a CORS no Program.cs, permitindo requisições de origens específicas para a API.

--- Neil Cummings/EncontrosApp/API/Program.cs
+++ Neil Cummings/EncontrosApp/API/Program.cs
@@ -8,8 +8,11 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
