18dcd0f26e1cb5a04a51f71bbc2369f63cb60f13
Danilo Silva <danilo.silva@msn.com>
2025-07-06 12:26:45 -0300

    feat: Implementar geração de tokens JWT no TokenService
    - Adicionadas referências ao Microsoft.IdentityModel.Tokens e System.IdentityModel.Tokens.Jwt no projeto.
    - Implementada a lógica para criação de tokens JWT no método CriarToken do TokenService, incluindo validações para a chave do token e configuração de claims.
    - O token gerado tem validade de 7 dias e utiliza HMACSHA512 para assinatura, melhorando a segurança da autenticação.

--- Neil Cummings/EncontrosApp/API/Services/TokenService.cs
+++ Neil Cummings/EncontrosApp/API/Services/TokenService.cs
@@ -1,12 +1,43 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using API.Entities;
 using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services;
 
-public class TokenService : ITokenService
+public class TokenService(IConfiguration configuration) : ITokenService
 {
     public string CriarToken(Usuario usuario)
     {
-        return string.Empty;
+        var tokenKey = configuration["TokenKey"] ?? throw new Exception("TokenKey não configurado.");
+
+        if (tokenKey.Length < 64)
+        {
+            throw new Exception("TokenKey deve ter pelo menos 64 caracteres.");
+        }
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.Email, usuario.Email),
+            new(ClaimTypes.NameIdentifier, usuario.Id),
+        };
+
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddDays(7),
+            SigningCredentials = credentials,
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
     }
 }
