0506b1f1e49164bd80e63127efc18b0c8efd31ed
Danilo Silva <danilo.silva@msn.com>
2025-07-06 12:14:16 -0300

    feat: Adicionar serviço de token e interface para autenticação
    - Criada a interface ITokenService com o método CriarToken para geração de tokens de autenticação.
    - Implementado o TokenService que implementa a interface ITokenService, retornando um token vazio por enquanto.
    - Registrado o TokenService no contêiner de injeção de dependência no Program.cs, permitindo sua utilização na aplicação.

---
+++ Neil Cummings/EncontrosApp/API/Services/TokenService.cs
@@ -0,0 +1,12 @@
+using API.Entities;
+using API.Interfaces;
+
+namespace API.Services;
+
+public class TokenService : ITokenService
+{
+    public string CriarToken(Usuario usuario)
+    {
+        return string.Empty;
+    }
+}
