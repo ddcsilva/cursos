bdee8c9abaad67e456e715e7de0cfbcce8733caf
Danilo Silva <danilo.silva@msn.com>
2025-07-05 22:40:00 -0300

    feat: Adicionar colunas de segurança na tabela Usuarios
    - Criadas migrações para adicionar as colunas SenhaHash e SenhaSalt na tabela Usuarios, melhorando a segurança do armazenamento de senhas.
    - Atualizado o modelo de dados do usuário para incluir as novas propriedades de segurança.

---
+++ Neil Cummings/EncontrosApp/API/Data/Migrations/20250706013609_AlteracaoUsuario.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlteracaoUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "SenhaHash",
+                table: "Usuarios",
+                type: "BLOB",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "SenhaSalt",
+                table: "Usuarios",
+                type: "BLOB",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SenhaHash",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "SenhaSalt",
+                table: "Usuarios");
+        }
+    }
+}
--- Neil Cummings/EncontrosApp/API/Entities/Usuario.cs
+++ Neil Cummings/EncontrosApp/API/Entities/Usuario.cs
@@ -5,4 +5,6 @@ public class Usuario
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public required string NomeExibicao { get; set; }
     public required string Email { get; set; }
+    public required byte[] SenhaHash { get; set; }
+    public required byte[] SenhaSalt { get; set; }
 }
