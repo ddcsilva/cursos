d1e80e051dda7bbef7931c4b082f82bba76b97f4
Danilo Silva <danilo.silva@msn.com>
2025-07-13 18:31:08 -0300

    feat: Adicionar endpoint de transações e modelo de requisição
    - Modificado o endpoint raiz para retornar uma mensagem em /v1/transactions.
    - Adicionada a classe Request com propriedades para representar os dados de entrada das transações.

--- Balta/3054/Dima/Dima.Api/Program.cs
+++ Balta/3054/Dima/Dima.Api/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/v1/transactions", () => new { message = "Hello World!" });
 
 app.Run();
+
+// Request
+public class Request
+{
+    public string Title { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public int Type { get; set; }
+    public decimal Amount { get; set; }
+    public long CategoryId { get; set; }
+    public string UserId { get; set; } = string.Empty;
+}
+// Response
+// Handler
