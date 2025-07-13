aeab170861e7406034e612a435a9017493ae0141
Danilo Silva <danilo.silva@msn.com>
2025-07-13 18:07:15 -0300

    feat: Adicionar enumeração ETransactionType e atualizar modelo de Transação
    - Criada a enumeração ETransactionType no namespace Dima.Core.Enums, com os tipos Deposit e Withdraw.
    - Atualizada a classe Transaction para utilizar ETransactionType no campo Type, substituindo o tipo int por ETransactionType e definindo o valor padrão como Withdraw.

--- Balta/3054/Dima/Dima.Core/Models/Transaction.cs
+++ Balta/3054/Dima/Dima.Core/Models/Transaction.cs
@@ -1,3 +1,5 @@
+using Dima.Core.Enums;
+
 namespace Dima.Core.Models;
 
 public class Transaction
@@ -6,7 +8,7 @@ public class Transaction
     public string Title { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; } = DateTime.Now;
     public DateTime? PaidOrReceivedAt { get; set; }
-    public int Type { get; set; }
+    public ETransactionType Type { get; set; } = ETransactionType.Withdraw;
     public decimal Amount { get; set; }
     public long CategoryId { get; set; }
     public Category Category { get; set; } = null!;
