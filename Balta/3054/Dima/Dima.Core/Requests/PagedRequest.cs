98f126f95b77d7cfad84be568be89d0fe1889845
Danilo Silva <danilo.silva@msn.com>
2025-07-14 20:18:30 -0300

    feat: Tornar a classe PagedRequest abstrata e adicionar a classe BaseResponse
    - Alterada a classe PagedRequest para ser abstrata, permitindo que outras classes herdem suas propriedades de paginação.
    - Criada a nova classe BaseResponse, que encapsula dados de resposta, mensagem e status, facilitando o tratamento de respostas em requisições.

--- Balta/3054/Dima/Dima.Core/Requests/PagedRequest.cs
+++ Balta/3054/Dima/Dima.Core/Requests/PagedRequest.cs
@@ -1,6 +1,6 @@
 namespace Dima.Core.Requests;
 
-public class PagedRequest : BaseRequest
+public abstract class PagedRequest : BaseRequest
 {
     public int PageNumber { get; set; } = 1;
     public int PageSize { get; set; } = 25;
