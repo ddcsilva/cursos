86117fb6f4e9d12addf2a6bed3e03946a14af000
Danilo Silva <danilo.silva@msn.com>
2025-07-14 19:24:38 -0300

    feat: Aplicar configurações de mapeamento a partir do assembly no AppDbContext
    - Adicionada a chamada para modelBuilder.ApplyConfigurationsFromAssembly no método OnModelCreating da classe AppDbContext, permitindo que as configurações de mapeamento sejam carregadas automaticamente a partir do assembly.

--- Balta/3054/Dima/Dima.Api/Data/AppDbContext.cs
+++ Balta/3054/Dima/Dima.Api/Data/AppDbContext.cs
@@ -10,5 +10,6 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
     }
 }
