1673d2259e10dfbf319050c5b70b25670321b502
Danilo Silva <danilo.silva@msn.com>
2025-07-28 10:33:36 -0300

    feat: Criar programa para somar dois valores fornecidos pelo usuário
    - Adicionado um novo arquivo JavaScript que implementa um programa simples para receber dois valores e imprimir a soma deles.

---
+++ Matheus Battisti/Node do Zero a Maestria/01_INTRO/03_tarefa_01/programa.js
@@ -0,0 +1,8 @@
+// Tarefa 01
+// Crie um programa que receba dois valores do usuário e imprima a soma deles.
+
+const valor1 = 10;
+const valor2 = 20;
+const resultadoDaSoma = valor1 + valor2;
+
+console.log("O resultado da soma é:", resultadoDaSoma);
