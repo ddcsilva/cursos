89cd884a2a6255aed288822d0b99a68eb5f5f1b8
Danilo Silva <danilo.silva@msn.com>
2025-07-28 14:12:27 -0300

    feat: Adicionar arquivos de módulo para soma com importação e exportação
    - Criado o arquivo meu_modulo.mjs que define a função somar e a exporta.
    - Adicionado o arquivo index.mjs que importa a função somar e demonstra seu uso com exemplos.

---
+++ Matheus Battisti/Node do Zero a Maestria/02_FUNDAMENTOS/02_export_import/index.mjs
@@ -0,0 +1,6 @@
+// import é uma palavra reservada que permite importar um módulo
+// O import é feito com a extensão .mjs
+import somar from "./meu_modulo.mjs";
+
+somar(2, 3);
+somar(5, 10);
---
+++ Matheus Battisti/Node do Zero a Maestria/02_FUNDAMENTOS/02_export_import/meu_modulo.mjs
@@ -0,0 +1,7 @@
+function somar(a, b) {
+  console.log(a + b);
+}
+
+// Exportando a função somar
+// export default é uma palavra reservada que permite exportar uma função
+export default somar;
