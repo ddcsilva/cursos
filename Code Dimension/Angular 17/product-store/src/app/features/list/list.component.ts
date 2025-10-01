d5888c50027abb7531dfe8939ba3643b96ed9e69
Danilo Silva <danilo.silva@msn.com>
2025-10-01 16:27:58 -0300

    feat: Adicionar configuração de proxy e estrutura de dados para produtos
    - Adicionado arquivo proxy.conf.json para configuração de proxy durante o desenvolvimento.
    - Criado db.json com dados de exemplo para produtos, facilitando o desenvolvimento e testes.
    - Atualizado angular.json para incluir a configuração de proxy.
    - Modificado package.json para incluir a dependência json-server.
    - Atualizado app.config.ts para fornecer o HttpClient.
    - Alterado list.component.ts para buscar produtos da API e exibi-los na lista.
    - Atualizado list.component.html para renderizar a lista de produtos dinamicamente.

--- Code Dimension/Angular 17/product-store/src/app/features/list/list.component.ts
+++ Code Dimension/Angular 17/product-store/src/app/features/list/list.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   standalone: true,
   imports: [],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrl: './list.component.scss',
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  products: any[] = [];
+  http = inject(HttpClient);
 
+  ngOnInit(): void {
+    this.http.get<any[]>('/api/products').subscribe((products) => {
+      this.products = products;
+    });
+  }
 }
