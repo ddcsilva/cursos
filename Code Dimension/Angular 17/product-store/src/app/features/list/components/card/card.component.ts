3d66db5ca1151644feb733df182a1804369bca32
Danilo Silva <danilo.silva@msn.com>
2025-10-01 19:25:05 -0300

    feat: Adicionar funcionalidade de edição de produtos
    - Criado o componente EditComponent com template e estilo básicos.
    - Adicionada a rota 'edit' no arquivo app.routes.ts para navegação ao novo componente de edição.
    - Atualizado o ListComponent para incluir a funcionalidade de edição, permitindo navegação para o EditComponent ao clicar no botão "Editar".
    - Modificado o CardComponent para emitir um evento de edição ao clicar no botão correspondente.

--- Code Dimension/Angular 17/product-store/src/app/features/list/components/card/card.component.ts
+++ Code Dimension/Angular 17/product-store/src/app/features/list/components/card/card.component.ts
@@ -1,4 +1,10 @@
-import { Component, computed, input } from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  input,
+  Output,
+} from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { Product } from '../../../../shared/interfaces/product.interface';
@@ -12,5 +18,7 @@ import { Product } from '../../../../shared/interfaces/product.interface';
 })
 export class CardComponent {
   product = input.required<Product>();
+  @Output() edit = new EventEmitter<Product>();
+
   productTitle = computed(() => this.product().title);
 }
--- Code Dimension/Angular 17/product-store/src/app/features/list/list.component.ts
+++ Code Dimension/Angular 17/product-store/src/app/features/list/list.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ProductService } from '../../shared/services/product.service';
 import { Product } from '../../shared/interfaces/product.interface';
 import { CardComponent } from './components/card/card.component';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
@@ -15,10 +15,15 @@ import { MatButtonModule } from '@angular/material/button';
 export class ListComponent implements OnInit {
   products: Product[] = [];
   productsService = inject(ProductService);
+  router = inject(Router);
 
   ngOnInit(): void {
     this.productsService.getAll().subscribe((products) => {
       this.products = products;
     });
+  }
+
+  onEdit(): void {
+    this.router.navigateByUrl('/edit');
   }
 }
