c0a1aac8035a43201e80bcbcb7f9f38d3b245eb1
Danilo Silva <danilo.silva@msn.com>
2025-09-30 23:25:30 -0300

    feat: Implementar tela inicial e lista de compras no aplicativo
    - Adicionado componente de lista de compras com HTML e CSS para exibição de itens.
    - Atualizado o app.component.html para incluir lógica de navegação entre a tela inicial e a lista de compras.
    - Implementado estilos para a tela inicial, incluindo layout e botões de navegação.
    - Atualizado o app.routes.ts para gerenciar as rotas entre a tela inicial, calculadora e lista de compras.
    - Ajustado o styles.scss para garantir uma apresentação adequada da aplicação.

--- Dougllas Sousa/Angular/conceitos-basicos/src/app/app.component.ts
+++ Dougllas Sousa/Angular/conceitos-basicos/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { ListaComprasComponent } from './lista-compras/lista-compras.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CalculadoraComponent],
+  imports: [CalculadoraComponent, ListaComprasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  telaAtual: string = 'home';
+
+  abrirCalculadora() {
+    this.telaAtual = 'calculadora';
+  }
+
+  abrirListaCompras() {
+    this.telaAtual = 'lista-compras';
+  }
+
+  voltarParaHome() {
+    this.telaAtual = 'home';
+  }
+}
--- Dougllas Sousa/Angular/conceitos-basicos/src/app/app.routes.ts
+++ Dougllas Sousa/Angular/conceitos-basicos/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { ListaComprasComponent } from './lista-compras/lista-compras.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'calculadora',
+    component: CalculadoraComponent,
+  },
+  {
+    path: 'lista-compras',
+    component: ListaComprasComponent,
+  },
+];
