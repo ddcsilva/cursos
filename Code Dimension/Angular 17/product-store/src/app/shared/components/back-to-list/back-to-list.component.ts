a55fa27a1def01fe8dcd3b1b67390402f5dcf58b
Danilo Silva <danilo.silva@msn.com>
2025-10-02 12:43:08 -0300

    feat: Atualizar componente de navegação "Voltar" com novo estilo e ícone
    - Modificado o template HTML do componente BackToList para incluir um ícone e estilização aprimorada.
    - Adicionado novo arquivo SCSS para estilizar o componente, melhorando a apresentação e a usabilidade.
    - Atualizados os imports no componente para incluir o MatIconModule, necessário para o novo ícone.

--- Code Dimension/Angular 17/product-store/src/app/shared/components/back-to-list/back-to-list.component.ts
+++ Code Dimension/Angular 17/product-store/src/app/shared/components/back-to-list/back-to-list.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-back-to-list',
   standalone: true,
-  imports: [RouterLink, MatButtonModule],
+  imports: [RouterLink, MatButtonModule, MatIconModule],
   templateUrl: './back-to-list.component.html',
-  styleUrl: './back-to-list.component.scss'
+  styleUrl: './back-to-list.component.scss',
 })
-export class BackToListComponent {
-
-}
+export class BackToListComponent {}
