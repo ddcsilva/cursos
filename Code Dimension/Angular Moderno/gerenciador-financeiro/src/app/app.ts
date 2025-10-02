060d40c86be97a5bbc195a23439984bc74fee0e5
Danilo Silva <danilo.silva@msn.com>
2025-10-02 16:58:25 -0300

    feat: Implementar estrutura de layout com cabeçalho e roteamento

--- Code Dimension/Angular Moderno/gerenciador-financeiro/src/app/app.ts
+++ Code Dimension/Angular Moderno/gerenciador-financeiro/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { Layout } from './core/layout/layout';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Layout, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'gerenciador-financeiro';
