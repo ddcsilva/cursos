9af64e8b86e3e0d7698bd26caad1f263ad7e32d5
Danilo Silva <danilo.silva@msn.com>
2025-10-05 20:10:29 -0300

    feat: Adicionar componentes de cabeçalho e rodapé ao projeto Organo
    - Criados os componentes Cabecalho e Rodape, incluindo HTML e CSS para estrutura e estilo.
    - Atualizada a estrutura do app.component.html para incluir os novos componentes.
    - Adicionadas regras de estilo para garantir que as imagens do cabeçalho e rodapé sejam responsivas.

--- Alura/angular-19-componentes-signals-fluxo/organo/src/app/app.component.ts
+++ Alura/angular-19-componentes-signals-fluxo/organo/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CabecalhoComponent, RodapeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'organo';
