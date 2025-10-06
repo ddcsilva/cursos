cb7f6c3216103be0d9188e0f8571f0dfa6bdc063
Danilo Silva <danilo.silva@msn.com>
2025-10-06 12:24:08 -0300

    feat: Atualizar mock de livros e estilização dos componentes
    - Adicionado novos livros ao mock-livros.ts, incluindo 'E não sobrou nenhum' e 'O médico e o monstro'.
    - Implementadas novas classes CSS para os gêneros literários, como romance, mistério, fantasia, ficção científica e técnicos.
    - Modificado o HTML e CSS dos componentes Livro e GeneroLiterario para aplicar as novas classes de estilo e permitir a exibição dinâmica de gêneros.

--- Alura/angular-19-componentes-signals-fluxo/organo/src/app/components/genero-literario/genero-literario.component.ts
+++ Alura/angular-19-componentes-signals-fluxo/organo/src/app/components/genero-literario/genero-literario.component.ts
@@ -1,11 +1,11 @@
 import { Component, input } from '@angular/core';
 import { LivroComponent } from '../livro/livro.component';
-import { livros } from '../../mock-livros';
 import { GeneroLiterario } from '../livro/livro.interface';
+import { CommonModule, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-genero-literario',
-  imports: [LivroComponent],
+  imports: [CommonModule, LivroComponent, NgClass],
   templateUrl: './genero-literario.component.html',
   styleUrl: './genero-literario.component.css',
 })
