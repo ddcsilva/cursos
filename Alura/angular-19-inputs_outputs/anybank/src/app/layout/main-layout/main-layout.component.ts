1ac12b6fe8ba725a7dbf0d01a0dd14e53c025b51
Danilo Silva <danilo.silva@msn.com>
2025-10-06 19:38:33 -0300

    feat: Adicionar componente de formulário para transações no Anybank
    - Criado o componente FormComponent com HTML e CSS para gerenciar novas transações.
    - Implementada a estrutura do formulário, incluindo seleção de tipo de transação (depósito e saque).
    - Atualizado o layout principal para incluir o novo componente de formulário.

--- Alura/angular-19-inputs_outputs/anybank/src/app/layout/main-layout/main-layout.component.ts
+++ Alura/angular-19-inputs_outputs/anybank/src/app/layout/main-layout/main-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { FormComponent } from "../../features/transaction/form/form.component";
 
 @Component({
   selector: 'app-main-layout',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FormComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css',
 })
