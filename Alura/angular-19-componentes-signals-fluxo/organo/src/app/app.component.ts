31d26b9c53e834c48dd80d2cc68dfc34048a85a9
Danilo Silva <danilo.silva@msn.com>
2025-10-06 10:33:36 -0300

    feat: Adicionar componente GeneroLiterario ao projeto Organo
    - Criado o componente GeneroLiterario com HTML e CSS para exibir informações sobre um gênero literário.
    - Atualizada a estrutura do app.component.html para incluir o novo componente GeneroLiterario.
    - Modificado o app.component.ts para importar e registrar o componente GeneroLiterario.
    - Adicionado arquivo de teste para o componente GeneroLiterario.

--- Alura/angular-19-componentes-signals-fluxo/organo/src/app/app.component.ts
+++ Alura/angular-19-componentes-signals-fluxo/organo/src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
 import { ListaLivrosComponent } from './components/lista-livros/lista-livros.component';
+import { GeneroLiterarioComponent } from './components/genero-literario/genero-literario.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CabecalhoComponent, RodapeComponent, ListaLivrosComponent],
+  imports: [
+    CabecalhoComponent,
+    RodapeComponent,
+    ListaLivrosComponent,
+    GeneroLiterarioComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
