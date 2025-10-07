88052ea5aa2df568b9a904cd35c60342f9f70fa1
Danilo Silva <danilo.silva@msn.com>
2025-10-07 16:00:21 -0300

    feat: Melhorar exibição e lógica de transações no componente TransactionItem
    - Atualizado o template do TransactionItemComponent para exibir a data da transação formatada e o valor da transação com base no tipo (saque ou depósito).
    - Modificado o modelo de transação para incluir a data da transação.
    - Implementada lógica computada para calcular o valor da transação, considerando o tipo de transação.

--- Alura/angular-19-inputs-outputs/anybank/src/app/features/transaction/components/transaction-history/transaction-item/transaction-item.component.ts
+++ Alura/angular-19-inputs-outputs/anybank/src/app/features/transaction/components/transaction-history/transaction-item/transaction-item.component.ts
@@ -1,13 +1,21 @@
-import { CurrencyPipe } from '@angular/common';
-import { Component, input } from '@angular/core';
-import { Transaction } from '../../../models/transaction.model';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { Component, computed, input } from '@angular/core';
+import { Transaction, TransactionType } from '../../../models/transaction.model';
 
 @Component({
   selector: 'app-transaction-item',
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, DatePipe],
   templateUrl: './transaction-item.component.html',
   styleUrl: './transaction-item.component.css',
 })
 export class TransactionItemComponent {
-  transaction = input<Transaction>();
+  transaction = input.required<Transaction>();
+
+  transactionValue = computed(() => {
+    if (this.transaction()?.type === TransactionType.WITHDRAW) {
+      return -this.transaction().value;
+    }
+
+    return this.transaction().value;
+  });
 }
