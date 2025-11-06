365773ffa517e6db7454d907c6790126372efcfb
Danilo Silva <danilo.silva@msn.com>
2025-11-05 22:11:17 -0300

    feat: Capturando entradas de usuário e exibindo mensagens na tela

--- Alura/dart-trabalhando-sintaxe-configuracao-projeto/meu_app/bin/meu_app.dart
+++ Alura/dart-trabalhando-sintaxe-configuracao-projeto/meu_app/bin/meu_app.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
 void main() {
-  print("Hello World");
+  print("Olá meu nome é Dart. Qual seu nome?");
+  var entrada = stdin.readLineSync();
+  print("Muito prazer, $entrada. Vamos fazer vários programas juntos!");
 }
